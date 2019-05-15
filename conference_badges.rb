b2e02bd05c3752d43040cf6e9d8bcb9373f78e2d
industrious-format-3756 <xavier.ruiz@yale.edu>
2019-05-15 02:32:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,12 @@
-def badge_maker(name = _____)
+def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(names)
+  messageArr = []
+  names.each do |name|
+    messageArr << badge_maker(name)
+  end
+  messageArr
+end
+    
